18728dd1de0e1c48b5f417c336576dd9ff69f9fb
-6 <1195132772@qq.com>
2021-07-21 22:59:33 +0800

    修改SAE-.js: 关闭SAE-check相关功能

--- SAE-.js
+++ SAE-.js
@@ -5,18 +5,18 @@ include("SAE-init.js");
 include("SAE-json.js");
 include("SAE-disp.js");
 include("SAE-disp-table.js");
-include("SAE-check.js");
-include("SAE-check-table.js");
+//include("SAE-check.js");
+//include("SAE-check-table.js");
 
 SAE.init();
-SAE.options._OrigInputType = true;
+//SAE.options._OrigInputType = true;
 var x=SAE.json.load(fs.readFileSync(process.argv.length>2?process.argv[2]:"project.json").toString());
 //SAE.json.debug(x,"");
 SAE.disp.proj(x);
 SAE.disp.disp();
 //SAE.disp.disperr();
-SAE.check.proj(x);
-SAE.check.debug();
+//SAE.check.proj(x);
+//SAE.check.debug();
 // console.log(require('util').inspect(Symbol,true,null,true));
 function include(x){
 	eval(fs.readFileSync(x).toString());
