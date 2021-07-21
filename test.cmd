555a39bf7369a0d37daaa159bc9b314e822bf6c3
-6 <1195132772@qq.com>
2021-07-21 21:18:46 +0800

    被ob改过的test.cmd

--- test.cmd
+++ test.cmd
@@ -1,10 +1,5 @@
 @echo off
-if "%~1" == "" (
-	for %%a in (test\*.sb3) do (
-		unzip -o "%%~fa" project.json || goto :stop
-		node SAE-.js
-	)
-) else (
+if not "%~1" == "" (
 	if exist "%~f1" (
 		unzip -o "%~f1" project.json || goto :stop
 	) else if exist "test\%~1.sb3" (
@@ -15,7 +10,6 @@ if "%~1" == "" (
 		echo 找不到 "%~1" 或者 "test\%~1.sb3" 或者 "test\%~1"。
 		goto :stop
 	)
-	node SAE-.js
 )
+node SAE-.js
 :stop
-pause
